87da8e66a17b99781dedeec3b88b1971f2d61d28
VohnsonV <132500134+VohnsonV@users.noreply.github.com>
2023-06-12 20:07:57 -0300

    Teste 12/06/2023

--- Cursos/OneBitCode/JavaScript/Exercício-3/velocidade.js
+++ Cursos/OneBitCode/JavaScript/Exercício-3/velocidade.js
@@ -1,9 +1,11 @@
+//vamos começar obtendo os nomes e velocidades dos dois veículos:
 const veiculo1 = prompt("Informe o nome do 1º veiculo:")
 const velocidade1 = prompt("Informe a velocidade do 1º veiculo:")
 
 const veiculo2 = prompt("Informe o nome do 2º veiculo:")
 const velocidade2 = prompt("Informe a velocidade do 2º veiculo:")
 
+//Agora vamos escrever um IF para exibir o resultado apropriado na tela:
 if (velocidade1 > velocidade2) {
   alert(veiculo1 + " é mais rápido do que " + veiculo2)
 } else if (velocidade2 > velocidade1) {
